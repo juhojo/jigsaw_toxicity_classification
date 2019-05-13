99bfcefed8df43289ac81eaf66064176823d143b
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-05-13 11:30:04 +0300

    minor fixes

--- notebook/visualizations.py
+++ notebook/visualizations.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def visualize_target_by_identity(data, columns):
+def visualize_toxicity_by_identity(data, columns):
     data.groupby(data["target"] >= .5).mean()[columns].T \
         .plot.barh()
 
-    plt.legend(title="Is offensive")
+    plt.legend(title="Is toxic")
 
     plt.tight_layout()
     plt.title("Comment summary – targeted identity")
